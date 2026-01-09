75b3f55359adf740941b3cd4889734e39cb41d97
jimgallagher80 <jimgallagher@me.com>
2026-01-09 21:13:37 +0000

    Create words.js

---
+++ wordladder/words.js
@@ -0,0 +1,11 @@
+// Small curated 4-letter dictionary (extend freely)
+const WORDS = [
+  "cold","cord","card","ward","warm",
+  "lead","load","goad","gold",
+  "head","heal","teal","tell","tall",
+  "spin","spit","spot","shot","shoe",
+  "hand","hard","harm","farm","form",
+  "book","boot","boon","moon","moan",
+  "rise","rode","code","coda",
+  "mate","made","mode","mole","mile"
+];
